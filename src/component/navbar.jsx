29f715336860fecc4ce3b353af5552154bfb9976
Zeezart <azeezhazeezah010@gmail.com>
2024-03-23 16:51:38 +0100

    fixed padding

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo.svg"
 
 function Navbar(){
     return(
-        <Box display="flex" justifyContent="space-between" alignItems="center" p="3rem 6rem">
+        <Box display="flex" justifyContent="space-between" alignItems="center" p={{sm: "3rem 3rem", md:"3rem 6rem", lg:"3rem 6rem"}}>
             <Image src={logo}/>
             <Box display="flex" gap="2rem">
                 <Text color="#A8AEBF" fontSize="1rem"pb="0.5rem" borderBottom="3px solid #7B6EF6">Home</Text>
